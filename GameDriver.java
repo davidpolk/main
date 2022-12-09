59d2ce9ac5ac8706d06b72bf44a2de0087a0a4a2
davidpolk <107951987+davidpolk@users.noreply.github.com>
2022-12-09 15:21:04 -0700

    Add files via upload

---
+++ GameDriver.java
@@ -0,0 +1,11 @@
+package RhythmGame.Screen;
+
+import java.io.IOException;
+
+//driver that creates a new screen object to begin playback of game
+public class GameDriver {
+	public static void main(String[] args) throws IOException
+	{
+		Screen gameScreen = new Screen("Rhythm Game");    
+	}
+}
